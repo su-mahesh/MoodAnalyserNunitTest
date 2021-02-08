1a67e8f2bed6d51c00ca765822173e966211014e
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-08 17:33:07 +0530

    Add project files.

---
+++ MoodAnalyserNUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MoodAnalyserNUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
